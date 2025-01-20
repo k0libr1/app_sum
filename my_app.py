ec46488e88065bf69a3a44eb93246775ddb5692a
Andrey Ushakov <ulllakov@google.com>
2025-01-20 23:54:18 +0500

    changed output format

--- my_app.py
+++ my_app.py
@@ -2,7 +2,7 @@ import sys
 
 def twosum(a, b):
     result = int(a) + int(b)
-    print(f'Сумма: {result}')
+    print(f'Сумма {a} и {b} = {result}')
 
 if __name__ == '__main__':
     a = sys.argv[1]
